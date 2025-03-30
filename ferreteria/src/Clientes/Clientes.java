ad62708c1039c307383db2df6025d08ba6ce8d5e
Brayan <braiancamilo0@gmail.com>
2025-03-30 12:00:20 -0500

    Creación del Javadoc de "Clientes.java"

--- ferreteria/src/Clientes/Clientes.java
+++ ferreteria/src/Clientes/Clientes.java
@@ -1,15 +1,38 @@
+/**
+ * Clase que representa a un cliente con atributos básicos como ID, nombre,
+ * teléfono, dirección y correo electrónico.
+ */
 package Clientes;
 
-public class Clientes
-{
-    int id_cliente;
-    String nombre;
-    String telefono;
-    String direccion;
-    String correo;
+/**
+ * Clase Clientes que almacena la información de un cliente.
+ */
+public class Clientes {
 
-    public Clientes(int id_cliente, String nombre, String telefono, String direccion, String correo)
-    {
+    /** Identificador único del cliente */
+    private int id_cliente;
+
+    /** Nombre del cliente */
+    private String nombre;
+
+    /** Teléfono del cliente */
+    private String telefono;
+
+    /** Dirección del cliente */
+    private String direccion;
+
+    /** Correo electrónico del cliente */
+    private String correo;
+
+    /**
+     * Constructor de la clase Clientes.
+     * @param id_cliente Identificador del cliente.
+     * @param nombre Nombre del cliente.
+     * @param telefono Teléfono del cliente.
+     * @param direccion Dirección del cliente.
+     * @param correo Correo electrónico del cliente.
+     */
+    public Clientes(int id_cliente, String nombre, String telefono, String direccion, String correo) {
         this.id_cliente = id_cliente;
         this.nombre = nombre;
         this.telefono = telefono;
@@ -17,66 +40,83 @@ public class Clientes
         this.correo = correo;
     }
 
-    public int getId_cliente()
-    {
+    /**
+     * Obtiene el identificador del cliente.
+     * @return ID del cliente.
+     */
+    public int getId_cliente() {
         return id_cliente;
     }
 
-    public void setId_cliente(int id_cliente)
-    {
+    /**
+     * Establece el identificador del cliente.
+     * @param id_cliente Nuevo ID del cliente.
+     */
+    public void setId_cliente(int id_cliente) {
         this.id_cliente = id_cliente;
     }
 
-    public String getNombre()
-    {
+    /**
+     * Obtiene el nombre del cliente.
+     * @return Nombre del cliente.
+     */
+    public String getNombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre)
-    {
+    /**
+     * Establece el nombre del cliente.
+     * @param nombre Nuevo nombre del cliente.
+     */
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    public String getTelefono()
-    {
+    /**
+     * Obtiene el teléfono del cliente.
+     * @return Teléfono del cliente.
+     */
+    public String getTelefono() {
         return telefono;
     }
 
-    public void setTelefono(String telefono)
-    {
+    /**
+     * Establece el teléfono del cliente.
+     * @param telefono Nuevo teléfono del cliente.
+     */
+    public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
 
-    public String getDireccion()
-    {
+    /**
+     * Obtiene la dirección del cliente.
+     * @return Dirección del cliente.
+     */
+    public String getDireccion() {
         return direccion;
     }
 
-    public void setDireccion(String direccion)
-    {
+    /**
+     * Establece la dirección del cliente.
+     * @param direccion Nueva dirección del cliente.
+     */
+    public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
 
-    public String getCorreo()
-    {
+    /**
+     * Obtiene el correo electrónico del cliente.
+     * @return Correo electrónico del cliente.
+     */
+    public String getCorreo() {
         return correo;
     }
 
-    public void setCorreo(String correo)
-    {
+    /**
+     * Establece el correo electrónico del cliente.
+     * @param correo Nuevo correo del cliente.
+     */
+    public void setCorreo(String correo) {
         this.correo = correo;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
