db74b1092cd9799641ac7f30f5c073848c584cd4
Brayan <braiancamilo0@gmail.com>
2025-03-30 12:56:18 -0500

    Añadida la función crear

--- ferreteria/src/Clientes/ClientesDAO.java
+++ ferreteria/src/Clientes/ClientesDAO.java
@@ -1,4 +1,36 @@
 package Clientes;
 
+import Conexion.ConexionBD;
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ClientesDAO {
+    private ConexionBD conexionBD = new ConexionBD();
+    public void crear(Clientes clientes){
+        Connection con = conexionBD.getConnection();
+
+        String query = "INSERT INTO clientes (nombre,telefono,direccion,correo) VALUES (?,?,?,?)";
+
+        try{
+            PreparedStatement pst = con.prepareStatement(query);
+
+            pst.setString(1,clientes.getNombre());
+            pst.setString(2, clientes.getTelefono());
+            pst.setString(3, clientes.getDireccion());
+            pst.setString(3, clientes.getCorreo());
+
+            int resultado = pst.executeUpdate();
+            if (resultado > 0){
+                JOptionPane.showMessageDialog(null,"Cliente creado con éxito.");
+            } else {
+                JOptionPane.showMessageDialog(null,"Error al crear al cliente.");
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
