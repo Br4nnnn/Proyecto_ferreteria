114eefe1b8df24b50f264cebc29148eef9727425
Brayan <braiancamilo0@gmail.com>
2025-04-03 12:17:07 -0500

    Creado el "actualizar" dentro del DAO de Ordenes_compra.

--- ferreteria/src/Ordenes_compra/Ordenes_compraDAO.java
+++ ferreteria/src/Ordenes_compra/Ordenes_compraDAO.java
@@ -8,20 +8,20 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Ordenes_compraDAO {
-    ConexionBD conexionDB = new ConexionBD();
+    ConexionBD conexionBD = new ConexionBD();
 
-    public void Crear(Ordenes_compra ordenesCompra){
-        Connection con = conexionDB.getConnection();
+    public void crear(Ordenes_compra ordenes_Compra){
+        Connection con = conexionBD.getConnection();
         String query = "INSERT INTO ordenes_compra (id_cliente, id_empleado, id_producto, total, estado_orden, fecha_compra) VALUES (?, ?, ?, ?, ?, ?)";
 
         try{
             PreparedStatement pst = con.prepareStatement(query);
-            pst.setInt(1, ordenesCompra.getId_cliente());
-            pst.setInt(2, ordenesCompra.getId_empleado());
-            pst.setInt(3, ordenesCompra.getId_producto());
-            pst.setInt(4, ordenesCompra.getTotal());
-            pst.setString(4, ordenesCompra.getEstado_orden());
-            pst.setString(4, ordenesCompra.getFecha_compra());
+            pst.setInt(1, ordenes_Compra.getId_cliente());
+            pst.setInt(2, ordenes_Compra.getId_empleado());
+            pst.setInt(3, ordenes_Compra.getId_producto());
+            pst.setInt(4, ordenes_Compra.getTotal());
+            pst.setString(4, ordenes_Compra.getEstado_orden());
+            pst.setString(4, ordenes_Compra.getFecha_compra());
 
             int resultado = pst.executeUpdate();
             String mensaje = resultado > 0 ? "Orden de compra registrada con éxito!" : "Error al ingresar la orden de compra...";
@@ -33,7 +33,7 @@ public class Ordenes_compraDAO {
     }
 
     public void actualizar(Ordenes_compra ordenes_Compra){
-        Connection con = conexionDB.getConnection();
+        Connection con = conexionBD.getConnection();
         String query = "UPDATE ordenes_compra SET id_cliente = ?, id_empleado = ?, id_producto = ?, total = ?, estado_compra = ?, fecha_compra = ?";
 
         try{
@@ -46,9 +46,9 @@ public class Ordenes_compraDAO {
             pst.setString(4, ordenes_Compra.getFecha_compra());
 
             int resultado = pst.executeUpdate();
-            String mensaje = resultado > 0 ? "Orden de compra actualizada con éxito!" : "Error al actualizar la orden de compra...";
-            int tipo_mensaje = resultado > 0 ? JOptionPane.INFORMATION_MESSAGE : JOptionPane.ERROR_MESSAGE;
-            JOptionPane.showMessageDialog(null, mensaje, "Ordenes compra:", tipo_mensaje);
+            String mensaje = resultado > 0 ? "La órden de compra ha sido actualizada con éxito." : "Error al actualizar la orden de compra.";
+            int mensaje_tipo = resultado > 0 ? JOptionPane.INFORMATION_MESSAGE : JOptionPane.ERROR_MESSAGE;
+            JOptionPane.showMessageDialog(null, mensaje, "Ordenes compra:", mensaje_tipo);
         } catch (SQLException e){
             e.printStackTrace();
         }
