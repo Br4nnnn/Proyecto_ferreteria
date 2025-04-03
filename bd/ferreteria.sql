ae966d3fc35563470423b04a1612af06a0767c97
Brayan <braiancamilo0@gmail.com>
2025-04-02 23:30:09 -0500

    Actualización de la base de datos.

--- bd/ferreteria.sql
+++ bd/ferreteria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-03-2025 a las 19:39:05
+-- Tiempo de generación: 03-04-2025 a las 06:29:20
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
