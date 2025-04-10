ece9a095e9e15e4d5db94f3d336b0333009c13de
Brayan <braiancamilo0@gmail.com>
2025-04-10 04:20:31 -0500

    Actualizada la base de datos.

--- bd/ferreteria.sql
+++ bd/ferreteria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-04-2025 a las 06:29:20
+-- Tiempo de generación: 10-04-2025 a las 11:16:21
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -29,11 +29,18 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `clientes` (
   `id_cliente` int(11) NOT NULL,
-  `nombre` varchar(50) DEFAULT NULL,
-  `telefono` varchar(50) DEFAULT NULL,
-  `direccion` varchar(50) DEFAULT NULL,
-  `correo` varchar(50) DEFAULT NULL
+  `nombre` varchar(50) NOT NULL,
+  `telefono` varchar(50) NOT NULL,
+  `direccion` varchar(50) NOT NULL,
+  `correo` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`id_cliente`, `nombre`, `telefono`, `direccion`, `correo`) VALUES
+(1, 'andres', '235412345', '4123453', 'greasrgh');
 
 -- --------------------------------------------------------
 
@@ -43,9 +50,9 @@ CREATE TABLE `clientes` (
 
 CREATE TABLE `empleados` (
   `id_empleado` int(11) NOT NULL,
-  `nombre` varchar(50) DEFAULT NULL,
+  `nombre` varchar(50) NOT NULL,
   `cargo` enum('administrador','vendedor') DEFAULT NULL,
-  `salario` float(8,2) DEFAULT NULL
+  `salario` float(8,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -56,11 +63,11 @@ CREATE TABLE `empleados` (
 
 CREATE TABLE `inventario_productos` (
   `id_producto` int(11) NOT NULL,
-  `categoria_producto` enum('herramienta','electricos') DEFAULT NULL,
-  `nombre_producto` varchar(50) DEFAULT NULL,
-  `precio_proveedor` float(8,2) DEFAULT NULL,
-  `precio_venta` float(8,2) DEFAULT NULL,
-  `cantidad_stock` int(11) DEFAULT NULL,
+  `categoria_producto` enum('herramientas','tornillos','tuercas','pintura','construccion','fontaneria','electronica y cableado','adhesivos','decoracion') NOT NULL,
+  `nombre_producto` varchar(50) NOT NULL,
+  `precio_proveedor` float(8,2) NOT NULL,
+  `precio_venta` float(8,2) NOT NULL,
+  `cantidad_stock` int(11) NOT NULL,
   `id_proveedor` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -72,11 +79,12 @@ CREATE TABLE `inventario_productos` (
 
 CREATE TABLE `ordenes_compra` (
   `id_orden_compra` int(11) NOT NULL,
-  `id_cliente` int(11) DEFAULT NULL,
-  `id_empleado` int(11) DEFAULT NULL,
-  `id_producto` int(11) DEFAULT NULL,
-  `estado_orden` enum('pendiente','pagada','enviada') DEFAULT NULL,
-  `fecha_compra` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `id_cliente` int(11) NOT NULL,
+  `id_empleado` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `estado_orden` enum('pendiente','pagada','enviada') NOT NULL,
+  `fecha_compra` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `total` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -87,8 +95,9 @@ CREATE TABLE `ordenes_compra` (
 
 CREATE TABLE `proveedores` (
   `id_proveedor` int(11) NOT NULL,
-  `nombre` varchar(50) DEFAULT NULL,
-  `contacto` varchar(50) DEFAULT NULL
+  `nombre` varchar(50) NOT NULL,
+  `contacto` varchar(50) NOT NULL,
+  `categoria_producto` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -99,9 +108,14 @@ CREATE TABLE `proveedores` (
 
 CREATE TABLE `registro_ventas` (
   `id_venta` int(11) NOT NULL,
-  `id_orden_compra` int(11) DEFAULT NULL,
-  `total` float DEFAULT NULL,
-  `fecha_registro` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `id_orden_compra` int(11) NOT NULL,
+  `sub_total` int(11) NOT NULL,
+  `fecha_venta` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `cantidad` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `precio_producto` int(20) NOT NULL,
+  `id_cliente` int(11) DEFAULT NULL,
+  `id_empleado` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -147,7 +161,50 @@ ALTER TABLE `proveedores`
 --
 ALTER TABLE `registro_ventas`
   ADD PRIMARY KEY (`id_venta`),
-  ADD KEY `id_orden_compra` (`id_orden_compra`);
+  ADD KEY `id_orden_compra` (`id_orden_compra`),
+  ADD KEY `id_producto` (`id_producto`),
+  ADD KEY `id_cliente` (`id_cliente`),
+  ADD KEY `id_empleado` (`id_empleado`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `empleados`
+--
+ALTER TABLE `empleados`
+  MODIFY `id_empleado` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `inventario_productos`
+--
+ALTER TABLE `inventario_productos`
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ordenes_compra`
+--
+ALTER TABLE `ordenes_compra`
+  MODIFY `id_orden_compra` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  MODIFY `id_proveedor` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `registro_ventas`
+--
+ALTER TABLE `registro_ventas`
+  MODIFY `id_venta` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
